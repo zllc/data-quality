76b28289a899f5eaf93f01dcbe344a44c1b41b44
Sizhao Liu <sizhaoliu@talend.com>
2016-05-02 10:59:31 +0200

    Revert "feature complete TDQ-11437 support GENERATE_FROM_LIST_STRING function"
    This reverts commit 2662786f44f66be2795dc69aea1ec5670795205b.

--- dataquality-sampling/src/main/java/org/talend/dataquality/datamasking/Functions/GenerateFromList.java
+++ dataquality-sampling/src/main/java/org/talend/dataquality/datamasking/Functions/GenerateFromList.java
@@ -10,16 +10,12 @@
 // 9 rue Pages 92150 Suresnes, France
 //
 // ============================================================================
-package org.talend.dataquality.datamasking.functions;
+package org.talend.dataquality.datamasking.Functions;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.log4j.Logger;
+import org.talend.dataquality.datamasking.Function;
 
 /**
  * created by jgonzalez on 24 juin 2015. This function will modify the input by randomly selecting one of the values
@@ -28,71 +24,13 @@ import org.apache.log4j.Logger;
  */
 public abstract class GenerateFromList<T2> extends Function<T2> {
 
-    private static final long serialVersionUID = 8936060786451303843L;
-
     protected List<String> StringTokens = new ArrayList<>();
-
-    private File dataFile = null;
-
-    private final static Logger log = Logger.getLogger(GenerateFromList.class);
 
     protected void init() {
         StringTokens.clear();
-        // input file is exist
-        if (parameters.length == 1 && integerParam == 0 && fileExist()) {
-            StringTokens = loadDefaultFile();
-        } else {
-            for (String tmp : parameters) {
-                StringTokens.add(tmp.trim());
-            }
+        for (String tmp : parameters) {
+            StringTokens.add(tmp.trim());
         }
-    }
-
-    /**
-     * zshen Check that whether the file is exist
-     * 
-     * @return
-     */
-    private boolean fileExist() {
-        String filePath = getFileFullPath();
-        File file = new File(filePath);
-        if (file.isFile() && file.exists()) {
-            dataFile = file;
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * zshen Load the file from default one
-     * 
-     * @return
-     */
-    private List<String> loadDefaultFile() {
-        try {
-            String encoding = "UTF-8"; //$NON-NLS-1$
-            if (dataFile.isFile() && dataFile.exists()) {
-                InputStreamReader read = new InputStreamReader(new FileInputStream(dataFile), encoding);
-                BufferedReader bufferedReader = new BufferedReader(read);
-                String lineTxt = null;
-                while ((lineTxt = bufferedReader.readLine()) != null) {
-                    StringTokens.add(lineTxt.trim());
-                }
-                read.close();
-            }
-        } catch (Exception e) {
-            log.error(e, e);
-        }
-        return StringTokens;
-    }
-
-    /**
-     * DOC zshen Comment method "getFileFullPath".
-     * 
-     * @return
-     */
-    protected String getFileFullPath() {
-        return "data/" + parameters[0]; //$NON-NLS-1$
     }
 
 }
